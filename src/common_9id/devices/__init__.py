63f98c8d637f3ca25a57adfd1c258d7bcc7aff63
mdwyman <mwyman@anl.gov>
2025-04-23 13:22:42 -0500

    Added HHL Apertures; updated common config

--- src/common_9id/devices/__init__.py
+++ src/common_9id/devices/__init__.py
@@ -2,3 +2,5 @@
 
 from .avs_filters import AVSfilters
 from .jj_transfocators import JJtransfocator1x, JJtransfocator2x
+from .jj_transfocators import JJtransfocator1xZ, JJtransfocator2xZ
+from .hhl_apertures import HHLAperture
