bf3f70d0575da763a01db0d710f7e664537c9002
mdwyman <mwyman@anl.gov>
2025-08-01 10:39:38 -0500

    Added metadata/baseline data to waxs, xpcs, and cssi (was already in saxs)

--- src/gixpcs/startup.py
+++ src/gixpcs/startup.py
@@ -74,3 +74,40 @@ else:
 
 
 RE(make_devices(clear=False))
+
+#-----------------------------------------------------------------------
+# Customized tools for 9id
+#
+# -Adding metadata PVs to own stream
+# -Adding baseline-labeled objects to baseline stream
+#
+#-----------------------------------------------------------------------
+
+# Record metadata PVs
+if iconfig.get("METADATA_PV", {}).get("ENABLE", False):
+    from common_9id.callbacks.metadataPVs import metadataPV_start_preprocessor
+    logger.info("Adding metadata PVs to own stream")
+    
+    try:
+        RE.preprocessors.append(metadataPV_start_preprocessor)
+    except Exception:
+        logger.warning("Could not load support to log metadata PVs")
+
+# Beamline configuration stored before/after experiment
+# uses baseline label to add to baseline data
+if iconfig.get("BASELINE_LABEL", {}).get("ENABLE", False):
+    logger.info("Adding baseline-labeled objects to baseline stream")
+    try:
+        baseline = oregistry.findall("baseline", allow_none=True) or []
+        for b in baseline:
+            try:
+                b.wait_for_connection()
+                sd.baseline.append(b)
+            except:
+                logger.info("%s not connected and not added to baseline", b.name)
+    except Exception as error:
+        print(
+            "\n"
+            f"Could not create baseline stream for RE. {error=}\n"
+        )
+        logger.warning("Could not add baseline-labeled objects to baseline stream")
