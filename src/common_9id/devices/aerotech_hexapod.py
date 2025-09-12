23c6214f4cb4d5f301921a31748bc9ac6a2e99d5
mdwyman <mwyman@anl.gov>
2025-09-12 12:48:58 -0500

    Fixed typo in aerotech hexapod device

--- src/common_9id/devices/aerotech_hexapod.py
+++ src/common_9id/devices/aerotech_hexapod.py
@@ -29,13 +29,14 @@ from ophyd import Device
 from ophyd import EpicsSignal, EpicsSignalRO
 from ophyd import EpicsMotor
 from ophyd import PVPositioner
+import time as ttime
 
 logger = logging.getLogger(__name__)
 logger.info(__file__)
     
 class aerotechHexapod(Device):
     '''
-    Handles single transfocator system
+    Handles single hexapod system
     
     '''
     def __init__(
@@ -57,6 +58,9 @@ class aerotechHexapod(Device):
         self._ax_motor = prefix+ax
         self._ay_motor = prefix+ay
         self._az_motor = prefix+az
+        
+        self.taskState = '' 
+        
         super().__init__(prefix, *args, **kwargs)
 
     x = FCpt(EpicsMotor,"{_x_motor}" , kind="hinted", labels={"motors"})
@@ -73,3 +77,57 @@ class aerotechHexapod(Device):
     strut5 = Cpt(EpicsSignalRO, "strut5" , kind="hinted", labels={"motors"})
     strut6 = Cpt(EpicsSignalRO, "strut6" , kind="hinted", labels={"motors"})
     
+    command = Cpt(EpicsSignal, "ExecuteCommand", string = True)
+    
+    def send_command(self, command):
+        self.command.put(command)
+        self.check_status(command)
+
+    def check_status(self, command):
+        self.taskState = self.command.get()
+        while self.taskState != command:
+            ttime.sleep(0.5)
+            self.taskState = self.command.get()
+        print('Hexapod command complete: ' + command)
+
+            
+    def set_work_coordinates(self, x, y ,z, a, b, c):
+        self.send_command(f'SetBaseToWork({x}, {y}, {z}, {a}, {b}, {c})')
+
+    def set_tool_coordinates(self, x, y ,z, a, b, c):
+        self.send_command(f'SetToolPoint(1, "Tool1",{x}, {y}, {z}, {a}, {b}, {c})')
+
+    def ackall(self):
+        self.send_command('AcknowledgeAll()')
+
+    def disable(self):
+        self.send_command('DisableHexapod()')
+
+    def enable_tool(self):
+        self.send_command('EnableTool()')
+        
+    def activate(self):
+        self.send_command('ActivateTool("Tool1")')
+
+    def setup(self, x = 0, y = 0, z = 0, a = 0, b = 0, c = 0,
+              x_offset = 0, y_offset = 0, z_offset = 0,
+              a_offset = 0, b_offset = 0, c_offset = 0):
+        
+        # acknowledge all
+        self.ackall()
+
+        # disable hexapod
+        self.disable()
+
+        # Set work coordinates
+        self.set_work_coordinates(x, y ,z, a, b, c)
+        
+        # Enable Tool
+        self.enable_tool()
+
+        # Set tool coordinates
+        self.set_tool_coordinates(x_offset, y_offset, z_offset, 
+                                  a_offset, b_offset, c_offset)
+
+        # activate tool
+        self.activate()
